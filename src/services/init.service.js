5207a9f3f4efef02dfe38bd0aef89fb49f703687
Marta Morán <martamoranl06@gmail.com>
2023-02-19 03:10:35 +0100

    last commit

--- src/services/init.service.js
+++ src/services/init.service.js
@@ -3,9 +3,9 @@ import axios from 'axios'
 class InitAxios {
     constructor(path) {
         this.api = axios.create({
-            baseURL: `http://localhost:5005/${path}`
+            baseURL: `http://instag-server-production.up.railway.app/${path}`
         })
-        
+
     }
 }
 
