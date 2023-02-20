87db80a47481549aea372bda713ef1b1ab4d6858
Marta Morán <martamoranl06@gmail.com>
2023-02-20 12:20:23 +0100

    avatar

--- src/components/DetailsComponent/Details.jsx
+++ src/components/DetailsComponent/Details.jsx
@@ -18,7 +18,7 @@ function Details() {
     const { user } = useContext(AuthContext)
     const [post, setPost] = useState(
         {
-            author: { name: '', image },
+            author: { name: '', avatar },
             image: '',
             title: '',
             description: ''
@@ -67,7 +67,7 @@ function Details() {
 
     return (
         <>
-            <Avatar src={post.author.image}></Avatar>
+            <Avatar src={post.author.avatar}></Avatar>
             <h4 className='mt-3'>{post.author.name}</h4>
             <img src={post.image} width="65%-"></img>
             {
